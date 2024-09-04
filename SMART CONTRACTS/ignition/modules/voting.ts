df210e200f3bac9d9c703fbfeb63852c550bcb91
psych0wh1z <ikeagudike@gmail.com>
2024-09-04 21:44:25 +0100

    Rename deployment script

--- SMART CONTRACTS/ignition/modules/voting.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const LockModule = buildModule("ZKVoteAfrica", (m) => {
-  const lock = m.contract("Lock");
-
-  return { lock };
-});
-
-export default LockModule;
